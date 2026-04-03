9fb8c7ed6c7631efa64511622bdd6c1c21e1b5fc
Vân Ca <202079096+alicia-vanca@users.noreply.github.com>
2026-04-03 17:43:28 +0700

    Fix username link visited issue and clean up code

--- src/main/java/com/app/common/config/DeviceIdManager.java
+++ src/main/java/com/app/common/config/DeviceIdManager.java
@@ -6,8 +6,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
