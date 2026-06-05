7c5f0441b4b7bd98478824b338f815ec5121c434
vietanh <lavietanh.utc@gmail.com>
2026-06-05 11:24:14 +0700

    fix position popup settings

--- src/main/java/com/app/admin/layout/controllers/AdminLayoutController.java
+++ src/main/java/com/app/admin/layout/controllers/AdminLayoutController.java
@@ -238,7 +238,7 @@ public class AdminLayoutController extends BaseLayoutController {
         settingsPopupHelper = new PreLoginSettingsPopupHelper(
                 "guest",
                 btnSettings,
-                PreLoginSettingsPopupHelper.PopupAnchorY.TOP,
+                PreLoginSettingsPopupHelper.PopupAnchorY.BOTTOM,
                 null,
                 this::reloadUI,
                 appUpdateController::onCheckUpdateManual,
